ba9f5f13b99ca0827365f26cd7189a728dbaf199
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-22 07:00:39 +0700

    Class: Field

--- bin/class.dart
+++ bin/class.dart
@@ -1,7 +1,16 @@
 void main() {
   Person person1 = Person();
 
-  print(person1);
+  person1.name = 'Taufan Prasetyo';
+  person1.address = 'Lampung';
+
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 }
 
-class Person {}
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+}
