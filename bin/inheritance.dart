32942fee1a3157bd1056dc1017dc98f820af5343
Taufan Prasetyo <taufanremote@gmail.com>
2024-08-22 15:15:56 +0700

    Class: Inheritance

---
+++ bin/inheritance.dart
@@ -0,0 +1,19 @@
+void main() {
+  var manager = Manager();
+  manager.name = 'Taufan';
+  manager.sayHello('Evita');
+
+  var vp = VicePresident();
+  vp.name = 'Taufan';
+  vp.sayHello('Evita');
+}
+
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print('Hello, $name, my name is ${this.name}');
+  }
+}
+
+class VicePresident extends Manager {}
